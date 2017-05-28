b316444d633cc1a122c41e9f2a1dfc5c4ae18c22
Nikhil <nikhilbhat22@gmail.com>
2017-05-28 19:09:42 +0530

    added exceptions

--- cpp/exceptionHandler.cpp
+++ cpp/exceptionHandler.cpp
@@ -1,7 +1,28 @@
 #include <iostream>
-#include <exception>
+#include <typeinfo>
+//#include <exception>
+//#include <math.h>
 
 using namespace std;
+
+class DynamicA
+{
+
+public:
+	DynamicA() {}
+	virtual ~DynamicA() {}
+};
+
+class DynamicB : public DynamicA
+{ };
+
+class DynamicC : public DynamicA
+{
+public:
+	int x;
+	DynamicC(): x(100) {}
+	void X() {cout << x << endl;}
+};
 
 struct DivideByZeroException : public std::exception
 {
@@ -11,26 +32,58 @@ struct DivideByZeroException : public std::exception
 	}
 };
 
+bool StringIsNumber(string in)
+{
+	for (string::iterator it = in.begin(); it != in.end(); ++it)
+	{
+		if (!isdigit(*it))
+			return false;
+	}
+	return true;
+}
+
 int main ()
 {
-	int d = 1;
-	int d1 = 0;	
-	int c;
+	cout << " Enter an integer : ";
+	int d1;
+	string in;
+	getline(cin, in);
+
+	DynamicA* A = new DynamicB();
 
 	try
 	{
-		if ( d1 == 0)
+		if (!StringIsNumber(in))
+			throw "Exception: Value not a number";
+
+		d1 = atoi (in.c_str());
+		int d = 1;
+		int c;
+
+		if (d1 == 0)
 			throw DivideByZeroException();
+		else if (d1 == 10)
+			throw 1;
 
 		c = d/d1;
+		DynamicC& C = dynamic_cast<DynamicC&> (*A);
+		C.X();
 	}
 	catch (const char* ex)
 	{
-		cout << " catching an exception " << ex << endl;
+		cout << ex << endl;
 	}
 	catch (DivideByZeroException ex)
 	{
-		cout <<  ex.what() << endl;
+		cout << ex.what() << endl;
+	}
+	catch (int t)
+	{
+		cout << "integer exception caught " << endl;
+	}
+	catch (std::bad_cast c)
+	{
+		cout << c.what() << endl;
 	}
 	cout << "running " << endl;
 }
