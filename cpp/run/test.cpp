6c9be497bedb7b0f2ea824c95c2946b7c5d79baa
Nikhil <nikhilbhat22@gmail.com>
2017-06-12 20:35:33 +0530

    factory pattern

--- cpp/run/test.cpp
+++ cpp/run/test.cpp
@@ -3,12 +3,40 @@
 #include "SQueue.hxx"
 #include <ctime>
 #include <cstdlib>
+#include <vector>
 
 void runSQueue();
 
 int main()
 {
-	runSQueue();
+	std::vector<int> vec;
+	std::cout << " vec capacity is " << vec.capacity() << std::endl;
+	vec.reserve(10);
+	vec.push_back(1);
+	std::cout << " vec capacity is " << vec.capacity() << std::endl;
+	vec[14] = 5;
+	vec[0] = 9;
+	std::cout << " vec capacity is " << vec.capacity() << std::endl;
+	std::cout << " vec size is " << vec.size() << std::endl;
+	std::cout << " vec element is " << vec.at(0) << std::endl;
+
+	int arr[] = {1,2,3};
+	std::cout << " arr size is " << sizeof(arr)/sizeof(arr[0]) << " elem size : " << sizeof(int) << std::endl;
+	
+	int overflow = 2147483650;
+	std::cout << " overflow " << overflow << std::endl;
+
+	char c = 65;
+	char ch = 'A';
+	int ascii = ch;
+	std::cout << " c " << c << std::endl;
+	std::cout << " ascii " << ascii << std::endl;
+
+	std::vector<int> vecFrmArr(arr,arr+3);
+	for (std::vector<int>::iterator it = vecFrmArr.begin(); it != vecFrmArr.end(); ++it)
+		std::cout << *it << " ";
+
+	std::cout << std::endl;
 	return 1;
 }
 
